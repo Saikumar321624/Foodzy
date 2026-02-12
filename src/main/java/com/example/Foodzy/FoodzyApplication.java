53b4f8f8f4483cd8f8c6abcaa8ad61d3471ce390
Saikumar321624 <saik826633@gmail.com>
2026-02-12 18:32:56 +0530

    first commit

---
+++ src/main/java/com/example/Foodzy/FoodzyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Foodzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Foodzy/FoodzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Foodzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
