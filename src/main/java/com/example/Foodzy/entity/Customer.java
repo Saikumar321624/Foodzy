c35b7f7c79d8b7e5077785ce60a67b48ad8eb77b
Karthikreddy3211 <mandakarthikreddy123@gmail.com>
2026-02-13 11:55:22 +0530

    2 class done

--- src/main/java/com/example/Foodzy/entity/Customer.java
+++ src/main/java/com/example/Foodzy/entity/Customer.java
@@ -1,6 +1,100 @@
 package com.example.Foodzy.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private Long mobileno;
+	private String mail;
+	private  String gender;
+	private  Address address;
+	@OneToMany
+	private List<Orders>orders;
+	@OneToMany
+	private List<Item>item;
+	public long getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(Long mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Orders> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Orders> orders) {
+		this.orders = orders;
+	}
+	public List<Item> getItem() {
+		return item;
+	}
+	public void setItem(List<Item> item) {
+		this.item = item;
+	}
+	public Customer(int id, String name, Long mobileno, String mail, String gender, Address address,
+			List<Orders> orders, List<Item> item) {
+		super();
+		
+		this.name = name;
+		this.mobileno = mobileno;
+		this.mail = mail;
+		this.gender = gender;
+		this.address = address;
+		this.orders = orders;
+		this.item = item;
+	}
+	public Customer() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", mobileno=" + mobileno + ", mail=" + mail + ", gender="
+				+ gender + ", address=" + address + ", orders=" + orders + ", item=" + item + "]";
+	}
+	
+	
+	
+	
+	
 	
 
 }
--- src/main/java/com/example/Foodzy/entity/Restaurant.java
+++ src/main/java/com/example/Foodzy/entity/Restaurant.java
@@ -1,5 +1,130 @@
 package com.example.Foodzy.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.criteria.Order;
+
+@Entity
 public class Restaurant {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private long mobileno;
+	private String mail;
+	private Address address;
+	@OneToMany
+	private List<Item> menu;
+	private String status;
+	private int rating;
+	private String description;
+	@OneToMany
+	private List<Order>orders;
+	private int packagingfee;
+	private String type;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(long mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Item> getMenu() {
+		return menu;
+	}
+	public void setMenu(List<Item> menu) {
+		this.menu = menu;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	public int getPackagingfee() {
+		return packagingfee;
+	}
+	public void setPackagingfee(int packagingfee) {
+		this.packagingfee = packagingfee;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Restaurant(long id, String name, long mobileno, String mail, Address address, List<Item> menu, String status,
+			int rating, String description, List<Order> orders, int packagingfee, String type) {
+		super();
+		
+		this.name = name;
+		this.mobileno = mobileno;
+		this.mail = mail;
+		this.address = address;
+		this.menu = menu;
+		this.status = status;
+		this.rating = rating;
+		this.description = description;
+		this.orders = orders;
+		this.packagingfee = packagingfee;
+		this.type = type;
+	}
+	public Restaurant() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Restaurant [id=" + id + ", name=" + name + ", mobileno=" + mobileno + ", mail=" + mail + ", address="
+				+ address + ", menu=" + menu + ", status=" + status + ", rating=" + rating + ", description="
+				+ description + ", orders=" + orders + ", packagingfee=" + packagingfee + ", type=" + type + "]";
+	}
+	
+	
 
 }
