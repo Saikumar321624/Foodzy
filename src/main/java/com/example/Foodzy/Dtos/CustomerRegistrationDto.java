2a8a1e91b95d6e917b7c7e7f727a0645f06bf7d4
gPavan-14 <pavanakashgoske@gmail.com>
2026-02-13 19:24:18 +0530

    ok

---
+++ src/main/java/com/example/Foodzy/Dtos/CustomerRegistrationDto.java
@@ -0,0 +1,5 @@
+package com.example.Foodzy.Dtos;
+
+public class CustomerRegistrationDto {
+
+}
--- src/main/java/com/example/Foodzy/Repositary/RestaurantRepo.java
+++ src/main/java/com/example/Foodzy/Repositary/RestaurantRepo.java
@@ -1,9 +1,10 @@
 package com.example.Foodzy.Repositary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.Foodzy.entity.Restaurant;
-
+@Repository
 public interface RestaurantRepo extends JpaRepository<Restaurant, Long>{
 	
 
