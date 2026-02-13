3c988502625c41455d53da4673ae186323a13f17
Saikumar321624 <saik826633@gmail.com>
2026-02-13 14:05:07 +0530

    repositorys

--- src/main/java/com/example/Foodzy/Repositary/DeliveryPartnerRepo.java
+++ src/main/java/com/example/Foodzy/Repositary/DeliveryPartnerRepo.java
@@ -3,8 +3,8 @@ package com.example.Foodzy.Repositary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.Foodzy.entity.Item;
+import com.example.Foodzy.entity.DeliveryPartner;
 @Repository
-public interface ItemRepo extends JpaRepository<Item, Long> {
+public interface DeliveryPartnerRepo extends JpaRepository<DeliveryPartner,Long> {
 
 }
--- src/main/java/com/example/Foodzy/Repositary/Vehicle.java
+++ src/main/java/com/example/Foodzy/Repositary/Vehicle.java
@@ -2,9 +2,7 @@ package com.example.Foodzy.Repositary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.Foodzy.entity.Item;
 @Repository
-public interface ItemRepo extends JpaRepository<Item, Long> {
+public interface Vehicle extends JpaRepository<Vehicle, Long>{
 
 }
