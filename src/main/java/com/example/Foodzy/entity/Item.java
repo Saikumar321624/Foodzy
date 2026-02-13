a708bdc9e7652168a561230ac26e19955fa5cfcf
gPavan-14 <pavanakashgoske@gmail.com>
2026-02-13 09:55:17 +0530

    ok

--- src/main/java/com/example/Foodzy/entity/Item.java
+++ src/main/java/com/example/Foodzy/entity/Item.java
@@ -1,9 +1,14 @@
 package com.example.Foodzy.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long itemId;
 	private String itemName;
 	private String description;
